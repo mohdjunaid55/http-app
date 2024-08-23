892ce6b984efbd4604e26331d8ddb654d18f210c
junaidrana5455@gmail.com <junaid5455@gmail.com>
2024-08-22 22:44:13 -0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,67 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
- let [todo, setTodo] = useState("")
- let [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-const getTodos = () => {
-  fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json")
-  .then((response)=> {
-    return response.json()
-  }).then((data) => {
-    // console.log(data);
-    if(typeof data === "object"){
-      let d = [];
-      for (const key in data) {
-      d.push(data[key]); 
-      }
-      setTodos([...d])
-    }
-  })
-  .catch(err => {
-    console.log(err);
-  })
-}
+  const getTodos = () => {
+    fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json")
+      .then((response) => response.json())
+      .then((data) => {
+        if (typeof data === "object") {
+          let d = [];
+          for (const key in data) {
+            d.push(data[key]);
+          }
+          setTodos(d);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const submit  = (event) => {
-  event.preventDefault();
+  const submit = (event) => {
+    event.preventDefault();
+    const newTodo = { text: todo, id: Date.now() };
 
-
-  fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json",{
-    method: "POST",
-    body: JSON.stringify(todo),
-    headers: {
-      "content-type": "application/json"
-    }
-  }).then((response)=> {
-    setTodo("")
-   setTodos([...todos, todo])
-    // console.log(response.json);
-  }).catch(err => {
-    console.log(err);
-  })
-  }
-
+    fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json", {
+      method: "POST",
+      body: JSON.stringify(newTodo),
+      headers: {
+        "Content-Type": "application/json", 
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodo("");
+        setTodos((prevTodos) => [...prevTodos, newTodo]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const deleteTodos = () => {
-    fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json",{
-      method: "DELETE"
+    fetch("https://backend-ced3e-default-rtdb.firebaseio.com/todo.json", {
+      method: "DELETE",
     })
-    .then((response)=> {
-      setTodos([])
-    }).catch(err => {
-      console.log(err);
-    })
-  }
+      .then((response) => {
+        setTodos([]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-
-
-  useEffect(()=> {
+  useEffect(() => {
     getTodos();
-  },[todos.length])
+  }, []);
 
   return (
     <div className="container mt-4">
       <form className="row g-3" onSubmit={submit}>
-        <div className="col-10 ">
+        <div className="col-10">
           <input
             type="text"
             className="form-control"
@@ -73,23 +70,34 @@ const submit  = (event) => {
             onChange={(event) => setTodo(event.target.value)}
           />
         </div>
-
-        <div class="col-2">
-          <button type="submit" className="btn btn-primary mb-3"disabled ={todo ? false: true} >Add </button>
+        <div className="col-2">
+          <button
+            type="submit"
+            className="btn btn-primary mb-3"
+            disabled={!todo}
+          >
+            Add
+          </button>
         </div>
       </form>
-
-<hr />
-        <button className="btn btn-danger" onClick={deleteTodos}>Delete Todos</button>
-<hr />
+      <hr />
+      <button className="btn btn-danger" onClick={deleteTodos}>
+        Delete Todos
+      </button>
+      <hr />
       <ul className="list-group">
-       {
-       todos.length > 0 ? todos.map((todo,index) =>(
-        <li className="list-group-item" key = {index}>{todo}</li>
-       )):<li className="list-group-item">No todo</li>
-       }
+        {todos.length > 0 ? (
+          todos.map((todo, index) => (
+            <li className="list-group-item" key={index}>
+              {todo.text}
+            </li>
+          ))
+        ) : (
+          <li className="list-group-item">No todo</li>
+        )}
       </ul>
     </div>
   );
 }
+
 export default App;
